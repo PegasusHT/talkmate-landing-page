ed08127210a8cb19951445511bf6d304758e9ae2
jimmybui <jimmybui1995@gmail.com>
2025-03-19 17:23:06 +0700

    changed insta link

--- public/constants/links.js
+++ public/constants/links.js
@@ -4,7 +4,7 @@
 
 const SOCIAL_MEDIA = {
     FACEBOOK_URL: 'https://www.facebook.com/profile.php?id=61565441774941',
-    INSTAGRAM_URL: 'https://www.instagram.com/talkmateai/',
+    INSTAGRAM_URL: 'https://www.instagram.com/talkmate.ai/',
     TWITTER_URL: 'https://x.com/TalkMateAI',
     TIKTOK_URL: 'https://www.tiktok.com/@talkmate_ai',
     YOUTUBE_URL: 'https://www.youtube.com/@TalkMateAI-youtube',
